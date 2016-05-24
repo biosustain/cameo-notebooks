7ce493402cf333554cdd689d423e38c2e2177fa1
Nikolaus Sonnenschein <niko.sonnenschein@gmail.com>
2016-05-24 09:16:55 +0200

    chore: skip high-level API notebook in tests

--- test_notebooks.sh
+++ test_notebooks.sh
@@ -3,10 +3,12 @@
 exit_code=0
 
 for file in *.ipynb; do
-    runipy $file
-    if [ $? != 0 ]; then
-        exit_code=$?
-        echo "Encountered problem with $file"
+    if [ $file != "08-high-level-API.ipynb" ] ; then
+        runipy $file
+        if [ $? != 0 ]; then
+            exit_code=$?
+            echo "Encountered problem with $file"
+        fi
     fi
 done
 
