a875d0adeeedb27468f59eff9783387296d9450b
Nikolaus Sonnenschein <niko.sonnenschein@gmail.com>
2016-06-28 16:49:21 +0200

    fix: skip 12-advanced-usage-of-heuristic-optimization

--- test_notebooks.sh
+++ test_notebooks.sh
@@ -3,7 +3,7 @@
 exit_code=0
 
 for file in *.ipynb; do
-    if [ $file != "08-high-level-API.ipynb" ] ; then
+    if [ $file != "08-high-level-API.ipynb" ] && [ $file != "12-advanced-usage-of-heuristic-optimization.ipynb" ] ; then
         runipy $file
         if [ $? != 0 ]; then
             exit_code=$?
